8fc958c7a751b3834f5d6e9aa6ff437dac0f243c
Tom Willis <twillis@topgunhq.com>
2013-11-07 08:49:23 -0500

    toggle fullscreen C-<f12>

--- etc/osx.el
+++ etc/osx.el
@@ -1,3 +1,13 @@
 (setq osx-packages '(exec-path-from-shell))
 (ensure-packages osx-packages)
 (exec-path-from-shell-initialize)
+
+
+(defun toggle-fullscreen ()
+  "Toggle full screen"
+  (interactive)
+  (set-frame-parameter
+     nil 'fullscreen
+     (when (not (frame-parameter nil 'fullscreen)) 'fullboth)))
+
+(global-set-key (kbd "C-<f12>") 'toggle-fullscreen)
