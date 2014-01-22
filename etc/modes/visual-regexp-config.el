82baf47aa14e3c4807c68bff25c86f32b69f9d6e
Tom Willis <twillis@topgunhq.com>
2014-01-21 21:11:42 -0500

    replace regex search with cool ones

--- etc/modes/visual-regexp-config.el
+++ etc/modes/visual-regexp-config.el
@@ -3,3 +3,5 @@
 (define-key global-map (kbd "C-c r") 'vr/replace)
 (define-key global-map (kbd "C-c q") 'vr/query-replace)
 (define-key global-map (kbd "C-c m") 'vr/mc-mark)
+(define-key esc-map (kbd "C-M-r") 'vr/isearch-backward) ;; C-M-r
+(define-key esc-map (kbd "C-M-s") 'vr/isearch-forward) ;; C-M-s
