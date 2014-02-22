c79aa885e111a2c32480ab7ebd7c919861d0eecd
Tom Willis <twillis@topgunhq.com>
2014-02-22 09:43:05 -0500

    force web-mode engine to something that wont blow up on angularjs

--- etc/modes/html-config.el
+++ etc/modes/html-config.el
@@ -1,9 +1,14 @@
 (setq html-config-packages '(
 			     emmet-mode
-			     web-mode))
+			     web-mode
+			     haml-mode
+			     flymake-haml))
 (ensure-packages html-config-packages)
 
 (add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
+
+(setq web-mode-engines-alist
+      '(("ctemplate"    . "\\.html\\'")))
 
 (add-hook 'web-mode-hook 'emmet-mode)
 (add-hook 'sgml-mode-hook 'emmet-mode)
