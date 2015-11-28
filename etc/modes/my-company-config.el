ddcf83a985a048768c95405aa3362a038c933004
Tom Willis <tom.willis@gmail.com>
2015-11-28 17:21:54 -0500

    this line is crucial

--- etc/modes/my-company-config.el
+++ etc/modes/my-company-config.el
@@ -1,4 +1,5 @@
 (setq company-config-packages '(company))
+(ensure-packages company-config-packages)
 (add-hook 'after-init-hook 'global-company-mode)
 
 (global-set-key (kbd "C-c .") 'company-complete)
